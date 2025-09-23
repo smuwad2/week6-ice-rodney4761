18929471f1824373fa2d3778df155088fd46f7f8
rodney4761 <rodney.wang.2024@smu.edu.sg>
2025-09-23 06:19:05 +0000

    Initial

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,23 +1,33 @@
 <script>
-    export default { 
-
-       // add code here
-
+    export default {
+        data() {
+            return {
+                loginType: "username"
+            }
+        }
     }
 </script>
 
 <template>
     <h1>Please select your login option</h1>
+
     <!-- Add/modify code in the following to have a dropdown list 
         which lets the user to select either to login with username or email -->
-    <select>
-        <option selected>Username login</option>
-        <option>Email login</option>
-    </select>
+
+        <select v-model="loginType">
+            <option selected value="username"> Username login</option>
+            <option> Email login</option>
+        </select>
+
+        <div v-if = 'loginType == "username"'>
+            <label>Username</label>
+            <input placeholder = "Enter your username">
+        </div>
+
+        <div v-else>
+            <label>Email</label>
+            <input placeholder = "Enter your email">
+        </div>
     
-    <div>
-        <label>Username</label>
-        <input placeholder="Enter your username">
-    </div>
 </template>
 
