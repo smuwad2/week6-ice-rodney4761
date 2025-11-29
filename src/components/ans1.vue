a41f27960ead5ec0b03f5e4419cfef3655cbbbb5
rodney4761 <rodney.wang.2024@smu.edu.sg>
2025-11-29 12:25:59 +0000

    ans

---
+++ src/components/ans1.vue
@@ -0,0 +1,37 @@
+<script>
+    export default {
+        data() {
+            return {
+                message: "Choose your favourite fruit:",
+                // fruit1: "apple",
+                // fruit2: "orange",
+                // fruit3: "peach",
+
+                fruits: ["apple", "orange", "peach"]
+                
+            }
+        }
+    }
+</script>
+
+<template>
+    <!-- <div id="app">
+        {{ message }} </br>
+        {{ fruit1 }} <input type="radio" name="fruit">
+        {{ fruit2 }} <input type="radio" name="fruit">
+        {{ fruit3 }} <input type="radio" name="fruit">
+    </div> -->
+
+    <h3>Using Array</h3>
+    {{  message }} </br>
+    <span v-for="fruit in fruits">
+        {{ fruit }} <input type = "radio" name = "fruit"></input>
+    </span>
+
+
+
+
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ans2.vue
@@ -0,0 +1,18 @@
+<script>
+    export default { 
+        data() {
+            return {
+                width: '200',
+                height: '200',
+                myImg: '/assets/me.png'
+            }
+        },
+    }
+</script>
+
+<template>
+    <!-- : is a short-hand notation for v-bind -->
+    <!-- <img v-bind:src="myImg" v-bind:width="width" v-bind:height="height"/> -->
+    <img :src="myImg" :width="width" :height="height"/>
+   
+</template>
